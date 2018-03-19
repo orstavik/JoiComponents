7194b7b6fff472af514424feac2773b6d4afb525
intertext.no <orstavik77@gmail.com>
2018-03-19 11:19:09 +0100

    cleaned up documentation. Added visibleChildren property.

--- src/ChildrenChangedMixin.js
+++ src/ChildrenChangedMixin.js
@@ -5,32 +5,39 @@ const childListChanged = Symbol("lightChildrenChanged");
 const checkVisibleChildrenChanged = Symbol("slotChildrenChanged");
 const listenForSlotChanges = Symbol("listenForSlotChanges");
 const onConnection = Symbol("onConnection");
+const visibleChildren = Symbol("visibleChildren");
 
 export const ChildrenChangedMixin = function (Base) {
   /**
-   * The ChildrenChangedMixin will trigger the .childrenChangedCallback(newVisibleChildren, oldVisibleChildren)
-   * on any subclass / Mixin that defines this method.
-   * The .childrenChangedCallback(newVisibleChildren, oldVisibleChildren) is a reactive function
-   * that triggers a callback every time a child in the immediate list of children changes.
+   * ChildrenChangedMixin adds a reactive lifecycle hook .childrenChangedCallback(...) to its subclasses.
+   * .childrenChangedCallback(...) is triggered every time
+   *   1) a child changes or
+   *   2) a slot that is a child changes.
+   *
+   * About 2) slotted children.
+   * If a <slot> element is a direct child of the element, the .childrenChangedCallback is
+   * triggered every time an element is either added or removed from the slot
+   * (ie. whenever a "slotchange" event is triggered on a slotted child element).
+   *
+   * ChildrenChangedMixin also adds a property .visibleChildren to its subclasses.
+   * .visibleChildren is the flattened list of:
+   *   a) all (first level) children, except slot,
+   *   b) the slotted children of the excluded slots
+   * The new and old visibleChildren are passed as the parameters of the
+   * .childrenChangedCallback(newVisibleChildren, oldVisibleChildren) method.
+   *
+   * Gold standard: https://github.com/webcomponents/gold-standard/wiki/
+   * a) Detachment: ChildrenChangedMixin always starts observing when it is connected to the DOM and stops when it is disconnected.
+   * b) Content assignment: changes to assignedNodes of slotted children are notified as if the change happened to a normal child.
    *
    * .childrenChangedCallback(...) is very similar to .attributeChangedCallback(...) in that
    *   a) it triggers a reactive function whenever a certain type of mutation of the element's lightDOM changes, and
    *   b) gives a reactive API to a facility that is already provided on the platform through the MutationObserver API.
    *
-   * Slotted children.
-   * If a <slot> element is set as child of the element, the .childrenChangedCallback will also be
-   * triggered everytime a new elements are slotted in that position or removed from that slot
-   * (ie. whenever a "slotchange" event is triggered on that slotted child element).
-   *
-   * The private property ._visibleChildren is the flattened list of:
-   *   a) all (first level) children, except slot,
-   *   b) the slotted children of the excluded slots
-   * It is recommended that subclasses handles the ._visibleChildren through the first parameter of the
-   * .childrenChangedCallback(newVisibleChildren, oldVisibleChildren).
-   *
-   * Gold standard: https://github.com/webcomponents/gold-standard/wiki/
-   * a) Detachment: ChildrenChangedMixin always starts observing when it is connected to the DOM and stops when it is disconnected.
-   * b) Content assignment: changes to assignedNodes of slotted children are notified as if the change happened to a normal child.
+   * .childrenChangedCallback(...) is triggered when a change occurs while the element is connected to the DOM.
+   * .childrenChangedCallback(...) is also called:
+   *  1) when the element is reconnected (disconnected and then reconnected to the DOM),
+   *  2) when the element is updated (when the constructor is called when the element has already been connected to the DOM).
    *
    * @param Base class extending HTMLElement
    * @returns {ChildrenChangedMixin}
@@ -39,16 +46,22 @@ export const ChildrenChangedMixin = function (Base) {
   return class ChildrenChangedMixin extends Base {
 
     /**
-     * This is the reactive life cycle function that is added to the HTMLElement.
      * Triggers when the list of visible children changes,
      * due to changes in list of children or a slotchange event of a child slot.
-     * Is always triggered on connectedCallback.
      *
      * @param newChildList the current list of visible children
      * @param oldChildList the previous list of visible children
      */
     childrenChangedCallback(newChildList, oldChildList) {
       if (super.childrenChangedCallback) super.childrenChangedCallback(newChildList, oldChildList);
+    }
+
+    get visibleChildren() {
+      return this[visibleChildren];
+    }
+
+    set visibleChildren(willThrowError) {
+      throw new Error("Illegal setter. element.visibleChildren can only be read, not set.");
     }
 
     constructor() {
@@ -82,11 +95,11 @@ export const ChildrenChangedMixin = function (Base) {
     }
 
     [checkVisibleChildrenChanged]() {
-      const oldChildList = this._visibleChildren;
-      const newChildList = ChildrenChangedMixin.getVisibleChildren(this);
+      const oldChildList = this[visibleChildren];
+      const newChildList = ChildrenChangedMixin.flattenVisibleChildren(this);
       if (ChildrenChangedMixin.arrayEquals(oldChildList, newChildList))
         return;
-      this._visibleChildren = newChildList;
+      this[visibleChildren] = newChildList;
       this.childrenChangedCallback(newChildList, oldChildList);
     }
 
@@ -108,7 +121,7 @@ export const ChildrenChangedMixin = function (Base) {
       return Array.from(host.children || []).filter((child) => child.constructor.name === "HTMLSlotElement");
     }
 
-    static getVisibleChildren(el) {
+    static flattenVisibleChildren(el) {
       let res = [];
       for (let i = 0; i < el.children.length; i++) {
         let child = el.children[i];
