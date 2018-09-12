6babdb7799c1a431876d15a712ce6f5a98f5fd2b
intertext.no <orstavik77@gmail.com>
2018-09-12 15:06:54 +0200

    microIDE

--- bookapp/MicroIDE/CodeExample2.js
+++ bookapp/MicroIDE/CodeExample2.js
@@ -3,62 +3,18 @@ function getSlotContent(slot) {
   return slot.assignedNodes().map(node => node.textContent).join("");
 }
 
-import hyperHTML from 'https://unpkg.com/hyperhtml?module';
 import {SlotchangeMixin} from '../../src/SlotchangeMixin.js';
-
-//this is a micro code editor
-export class MicroCodeEditor extends SlotchangeMixin(HTMLElement) {
-
-  constructor() {
-    super();
-    this.attachShadow({mode: "open"});
-    this.html = hyperHTML.bind(this.shadowRoot);
-    this._staticCodeFiles = [];
-    this.addEventListener("code-file-changed", () => this.render());
-    this.shadowRoot.addEventListener("code-mirror-box-change", () => this.runCode());
-  }
-
-  render() {
-    this.html`
-      <style></style>
-      <tabs-tabs>
-        ${this._staticCodeFiles.map(child => this.renderCodeFile(child))}
-        <tab-tab><code-runner></code-runner></tab-tab>
-      </tabs-tabs>`;
-  }
-
-  runCode() {
-    let codes = [];
-    let editors = this.shadowRoot.querySelectorAll("code-mirror-box");
-    for (let i = 0; i < editors.length; i++) {
-      let editor = editors[i];
-      codes.push({filename: editor.getAttribute("filename"), content: editor.getContent()});
-    }
-    if (codes.length)
-      this.shadowRoot.querySelector("code-runner").loadCodeSyncFromMap(codes);
-  }
-
-  renderCodeFile(codeFile) {
-    const {filename, filetype, content} = codeFile.getDataObj();
-    return hyperHTML.wire(codeFile)`<tab-tab name="${filename}"><code-mirror-box filename="${filename}">${content}</code-mirror-box></tab-tab>`
-  }
-
-  slotchangedCallback(name, newChildren, oldChildren) {
-    if (name !== "")
-      return;
-    this._staticCodeFiles = newChildren.filter(node => node.tagName && node.tagName === "CODE-FILE");
-    this.render();
-  }
-}
 
 export class CodeMirrorBox extends SlotchangeMixin(HTMLElement) {
   constructor() {
     super();
     this.attachShadow({mode: "open"});
     this.shadowRoot.innerHTML = `
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.37.0/codemirror.css">
-<style> b{ display: none; }</style><div></div><b><slot></slot></b>
-`;
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.37.0/codemirror.css">
+      <style> b{ display: none; }</style>
+      <div></div>
+      <b><slot></slot></b>`;
+
     this._slot = this.shadowRoot.children[3].children[0];
     this._codemirror = CodeMirror(this.shadowRoot.children[2], {mode: "javascript"});
     this._codemirror.on("change", data => this._dispatchChange(data));
