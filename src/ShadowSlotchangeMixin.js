e082e98702883c4205a1cd97a16c1bf47eac8bcf
intertext.no <orstavik77@gmail.com>
2018-09-14 12:52:39 +0200

    Updating ShadowSlotchangeMixin to latest and best version

--- src/ShadowSlotchangeMixin.js
+++ src/ShadowSlotchangeMixin.js
@@ -3,18 +3,27 @@
  *
  * Many thanks to Jan Miksovsky and the Elix project for input and inspiration.
  */
-
 import {flattenNodes} from "./flattenNodes.js";
 
-const firstConnected = new WeakSet();
-
-const slotchangeListener = Symbol("slotchangeListener");
-const triggerSlotchangeCB = Symbol("triggerSlotchangeCB");
-const slots = Symbol("slots");
-const assigneds = Symbol("assigneds");
+const isInit = Symbol("isInit");
+const init = Symbol("init");
+const processSlotchangeEvent = Symbol("processSlotchangeEvent");
+const processSlot = Symbol("processSlot");
+const slotToAssigned = Symbol("slotToAssigned");
+const microTaskRegister = Symbol("slotToAssigned");
 
 function arrayEquals(a, b) {
   return a && b && a.length === b.length && a.every((v, i) => v === b[i]);
+}
+
+function onlyOncePerMicroTaskCycle(register, key) {
+  if (register.has(key))
+    return false;
+  register.add(key);
+  Promise.resolve().then(() => {
+    register.delete(key);
+  });
+  return true;
 }
 
 export function ShadowSlotchangeMixin(Base) {
@@ -22,35 +31,70 @@ export function ShadowSlotchangeMixin(Base) {
 
     constructor() {
       super();
-      this[slotchangeListener] = (e) => this[triggerSlotchangeCB](e.currentTarget);
-      this[slots] = [];
-      this[assigneds] = new WeakMap();
+      this[isInit] = false;
+      this[microTaskRegister] = new WeakSet();
+      this[slotToAssigned] = new WeakMap();
     }
 
     connectedCallback() {
       if (super.connectedCallback) super.connectedCallback();
-      if (firstConnected.has(this))
-        return;
-      firstConnected.add(this);
-      this.shadowRoot.addEventListener("slotchange", this[slotchangeListener]);
-      const slots = this.shadowRoot.querySelector("slot");
-      if (!slots) return;
-      for (let i = 0; i < slots.length; i++)
-        this[triggerSlotchangeCB](slot);
+      this[isInit] || (this[isInit] = true, this[init]());
     }
 
-    // disconnectedCallback() {
-    //   if (super.disconnectedCallback) super.disconnectedCallback();
-    //   this.shadowRoot.removeEventListener("slotchange", this[slotchangeListener]);
-    // }
+    [init]() {
+      this.shadowRoot.addEventListener("slotchange", e => this[processSlotchangeEvent](e));
+      Promise.resolve().then(() => {
+        const slots = this.shadowRoot.querySelectorAll("slot");
+        if (!slots)
+          return;
+        if (this[slotToAssigned].has(slots[0]))           //abort operation if browser has
+          return;                                         //already run slotchange event (correctly)
+        for (let i = 0; i < slots.length; i++)
+          this[processSlot](slots[i]);
+      });
+    }
 
-    [triggerSlotchangeCB](slot) {
+    /**
+     * todo verify the presence of the bug and thus the need for this method.
+     * This method is likely needing to be called when adding a new slot element to the shadowDOM
+     * so to produce the initial slotchange event missing in Safari.
+     *
+     * @param slot the newly added slot in the shadowDOM which you wish to trigger the initial slotchange event on.
+     */
+    triggerSlotchangeCallbackManually(slot) {
+      Promise.resolve().then(() => this[processSlot](slot));
+    }
+
+    /**
+     * There is a bug in either the spec or Chrome in how slotchange events are processed that require handling:
+     * Sometimes you get multiple `slotchange` events for the same change of assignedNodes, sometimes you don't.
+     *
+     * When the slotchange event is triggered, it might also trigger from with the .target property
+     * being a chained <slot> and not the slot that is located within the shadowRoot of the element in which you
+     * listen for slotchange events.
+     *
+     * This problem means the following. When you get a slotchange event in,
+     * you need to ensure/find the slot that is from this shadowRoot.
+     * When you find this slot, you need to:
+     * 1. register this slot as processed,
+     * 2. process the slotchange event viewed from that slot perspective, and
+     * 3. after the microtask que has finished, mark the slot as ready to be processed again.
+     *
+     * @param e the slotchange event
+     */
+    [processSlotchangeEvent](e) {
+      const slot = e.path.find(n => n.tagName === "SLOT" && n.getRootNode() === this.shadowRoot);
+      if (onlyOncePerMicroTaskCycle(this[microTaskRegister], slot))
+        this[processSlot](slot);
+    }
+
+    [processSlot](slot) {
       let newAssigned = flattenNodes(slot.assignedNodes());
-      let oldAssigned = this[assigneds].get(slot);
-      if (arrayEquals(oldAssigned, newAssigned))
-        return;
-      this[assigneds].set(slot, newAssigned);
-      this.slotchangedCallback(slot.name, newAssigned, oldAssigned);
+      let oldAssigned = this[slotToAssigned].get(slot);
+      if (!arrayEquals(oldAssigned, newAssigned)) {
+        this[slotToAssigned].set(slot, newAssigned);
+        this.slotchangedCallback(slot.name, newAssigned, oldAssigned);
+      }
     }
   }
 }
