4a510ec67b8fea6d231d8a84f7950a12522c208e
intertext.no <orstavik77@gmail.com>
2018-10-06 00:41:41 +0200

    simplifying BatchedPostConstructorAndSlotchangeCB in SlotchangeMixin

--- src/slot/SlotchangeMixin.js
+++ src/slot/SlotchangeMixin.js
@@ -5,8 +5,8 @@
  */
 
 //pure function to find the last in toRun, that !hasRun
-function findLastNotChecked(toRun, hasRun){
-  for (let i = toRun.length - 1; i >= 0; i--){
+function findLastNotChecked(toRun, hasRun) {
+  for (let i = toRun.length - 1; i >= 0; i--) {
     let el = toRun[i];
     if (hasRun.indexOf(el) < 0)
       return el;
@@ -15,22 +15,25 @@ function findLastNotChecked(toRun, hasRun){
 }
 
 //Ques for batched tasks
-let startedQue =[];
+let startedQue = [];
 let completed = [];
 let isStarted = false;
 
 //First, block flushing of the que until DCL, and on DCL, open the que and try to flush it
 let dcl = document.readyState === "complete" || document.readyState === "loaded";
-dcl || window.addEventListener("DOMContentLoaded", function() {dcl = true; flushQue();});
+dcl || window.addEventListener("DOMContentLoaded", function () {
+  dcl = true;
+  flushQue();
+});
 
 //process for flushing que
-function flushQue(){
+function flushQue() {
   //step 1: check that dcl is ready.
   if (!dcl) return;
   //step 2: all elements started has been processed? reset and end
   const fnel = findLastNotChecked(startedQue, completed);
   if (!fnel) {
-    startedQue =[];
+    startedQue = [];
     completed = [];
     return;
   }
@@ -40,13 +43,15 @@ function flushQue(){
   flushQue();
 }
 
-function batchedConstructorCallback(fn, el){
-  startedQue.push([fn,el]);
-  if (!isStarted){
+function batchedConstructorCallback(fn, el) {
+  startedQue.push([fn, el]);
+  if (!isStarted) {
     isStarted = true;
-    Promise.resolve().then(()=>{
-      flushQue();
-      isStarted = false;
+    Promise.resolve().then(() => {
+      Promise.resolve().then(() => {
+        flushQue();
+        isStarted = false;
+      });
     });
   }
 }
@@ -56,7 +61,7 @@ function batchedConstructorCallback(fn, el){
  */
 function processSlotchange(e, el) {
   const path = e.composedPath();
-  if (path[0].getRootNode() === el.shadowRoot){
+  if (path[0].getRootNode() === el.shadowRoot) {
     e.stopPropagation();
     el.slotCallback(path[0]);
     return;
@@ -64,23 +69,21 @@ function processSlotchange(e, el) {
   for (let i = 0; i < path.length; i++) {
     if (path[i].tagName !== "SLOT")
       return;
-    if (path[i].parentNode === el && path[i+1].getRootNode() === el.shadowRoot) {
+    if (path[i].parentNode === el && path[i + 1].getRootNode() === el.shadowRoot) {
       e.stopPropagation();
-      el.slotCallback(path[i+1]);
+      el.slotCallback(path[i + 1]);
       return;
     }
   }
 }
 
-const initFn = function(el){
+const initFn = function (el) {
   const slots = el.shadowRoot.querySelectorAll("slot");
-  for (let i = 0; i< slots.length; i++)
+  for (let i = 0; i < slots.length; i++)
     el.slotCallback(slots[i]);
   if (slots.length === 0)      //adds a slotCallback(undefined) if none is available
     el.slotCallback(undefined);
-  Promise.resolve().then(()=>{
-    el.shadowRoot.addEventListener("slotchange", e => processSlotchange(e, el));
-  });
+  el.shadowRoot.addEventListener("slotchange", e => processSlotchange(e, el));
 };
 
 export function SlotchangeMixin(Base) {
