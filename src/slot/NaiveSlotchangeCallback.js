d9852d010922d207f51739b0bfa0b7e936fc5d5f
intertext.no <orstavik77@gmail.com>
2018-10-05 21:54:52 +0200

    betterment

---
+++ src/slot/NaiveSlotchangeCallback.js
@@ -0,0 +1,16 @@
+function findYourOwnSlot(e, shadowRoot){
+  const path = e.composedPath();
+  for(let i = 0; i < path.length -1; i++){
+    let node = path[i];
+    if (node.getRootNode() === shadowRoot)
+      return [node, i];
+  }
+  return null;
+}
+
+export function naiveSlotchangeCallback(el){
+  el.shadowRoot.addEventListener("slotchange", e => {   //[*]
+    const [slot, indirectness] = findYourOwnSlot(e);
+    el.slotchangeCallback(slot, indirectness, e);
+  });
+}
