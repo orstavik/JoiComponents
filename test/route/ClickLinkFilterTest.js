cbc2e5b87c4d7e558c2df567604674b9439dad3f
Maks Halochkin <33881747+Halochkin@users.noreply.github.com>
2018-11-15 19:05:49 +0200

    Update ClickLinkFilterTest.js

--- test/route/ClickLinkFilterTest.js
+++ test/route/ClickLinkFilterTest.js
@@ -1,149 +1,184 @@
-import {highjackLink} from "../../src/router/HashDotRouter.js";
+import {highjackLink, getBaseHref} from "../../src/router/HashDotRouter.js";
 
-const url = new URL("abc", document.baseURI).href;
-
-function createA() {
+function createA(attributes) {
   const a = document.createElement("a");
-  a.id = "test";
-  document.body.appendChild(a);
+  for (let key of Object.keys(attributes))
+    a.setAttribute(key, attributes[key]);
   return a;
 }
 
-function removeA() {
-  document.body.removeChild(document.querySelector("#test"));
-}
-
-function click(a, counter, detail) {
-  a.dispatchEvent(new MouseEvent("click" + counter, Object.assign({}, {bubbles: true, cancelable: true}, detail)));
+function click(a, detail) {
+  const event = new MouseEvent("click1", Object.assign({}, {bubbles: true, cancelable: true}, detail));
+  a.dispatchEvent(event);
 }
 
 describe("HighjackLink tests", () => {
 
-  it("Check the right link to be sure that wrong links will be skipped", (done) => {
-    let elem = createA();
-    window.addEventListener("click1", e => {
-      e.target.href = "abc";
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal("http://localhost:63342/Router/test/ClickLinkFilter/abc");
-      removeA();
-    });
-    click(elem, 1, null);
-    done();
+  it("Normal links within the base are not skipped", (done) => {
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      assert(result.endsWith("/abc"));
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({href: "abc"});
+    document.body.appendChild(element);
+    click(element, {href: "abc"});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("e.button", function (done) {
-    let elem = createA();
-    window.addEventListener("click2", e => {
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 2, {button: 1});
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({});
+    document.body.appendChild(element);
+    click(element, {button: 1});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("e.metaKey", function (done) {
-    let elem = createA();
-    window.addEventListener("click3", e => {
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 3, {metaKey: true});
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({});
+    document.body.appendChild(element);
+    click(element, {metaKey: true});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("e.ctrlKey", function (done) {
-    let elem = createA();
-    window.addEventListener("click4", e => {
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 4, {ctrlKey: true});
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({});
+    document.body.appendChild(element);
+    click(element, {ctrlKey: true});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("e.shiftKey", function (done) {
-    let elem = createA();
-    window.addEventListener("click5", e => {
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 5, {shiftKey: true});
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({});
+    document.body.appendChild(element);
+    click(element, {shiftKey: true});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("e.defaultPrevented", function (done) {
-    let elem = createA();
-    window.addEventListener("click6", e => {
+    const listener = e => {
       e.preventDefault();
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 6, null);
-    done();
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({href: "abc"});
+    document.body.appendChild(element);
+    click(element, {});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("download attribute", (done) => {
-    let elem = createA();
-    window.addEventListener("click7", e => {
-      elem.setAttribute("download", "file");
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 7, null);
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({download: "url"});
+    document.body.appendChild(element);
+    click(element, {});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("rel='external'", (done) => {
-    let elem = createA();
-    window.addEventListener("click8", e => {
-      elem.setAttribute("rel", "external");
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 8, null);
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({rel: "external"});
+    document.body.appendChild(element);
+    click(element, {});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("Href starts with a #", (done) => {
-    let elem = createA();
-    window.addEventListener("click9", e => {
-      e.target.href = "#abc";
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 9, null);
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({href: "#abc"});
+    document.body.appendChild(element);
+    click(element, {});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 
   it("Href starts with a mailto:", (done) => {
-    let elem = createA();
-    window.addEventListener("click10", e => {
-      e.target.href = "mailto:me";
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 10, null);
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({href: "mailto:abc"});
+    document.body.appendChild(element);
+    click(element, {});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
-
+// Does not work
   it("Empty href", (done) => {
-    let elem = createA();
-    window.addEventListener("click11", e => {
-      e.target.href = "";
-      let abc = highjackLink(e, url);
-      expect(abc).to.be.equal(undefined);
-      removeA();
-    });
-    click(elem, 11, null);
-    done();
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({href: " "});
+    document.body.appendChild(element);
+    click(element, {});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
+  });
+  it("Object href", (done) => {
+    const listener = e => {
+      const result = highjackLink(e, getBaseHref());
+      expect(result).to.be.equal(undefined);
+      done();
+    };
+    window.addEventListener("click1", listener);
+    const element = createA({href: {}});
+    document.body.appendChild(element);
+    click(element, {suka: SVGAnimatedString});
+    document.body.removeChild(element);
+    window.removeEventListener("click1", listener);
   });
 });
