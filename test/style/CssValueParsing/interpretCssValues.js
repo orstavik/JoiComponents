f5bf9f917508349f5dc8d36f89654a1bf1d3012c
intertext.no <orstavik77@gmail.com>
2019-03-23 15:59:09 +0100

    interpretCss value test file

---
+++ test/style/CssValueParsing/interpretCssValues.js
@@ -0,0 +1,8 @@
+import {interpretCssValue} from "../../../src/style/CssValueParser.js";
+
+describe("Interpret colorcalc", function () {
+  it("something", function () {
+    // const value= interpretCssValue("colorcalc(6px+ 6%)");
+    // expect(value).to.deep.equal("rgb(0,0,0)");
+  });
+});
